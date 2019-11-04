c978e33c1607eed834c05dd3fc64cdc07ebe37f1
kdvictor <huanliu_uestc@163.com>
2019-11-04 21:01:25 +0800

    纹理过滤方式

--- OpenGLPrimary/texture.cpp
+++ OpenGLPrimary/texture.cpp
@@ -33,7 +33,7 @@ void Texture::Init(const char* pImagePath)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); //放大缩小过滤方式：线性过滤
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);//纹理坐标0-1，超过部分的处理方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);//repeated模式：1.1和0.1取得像素一样，一次类推
 
 	//生成纹理并存入显存
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pBmpData);
